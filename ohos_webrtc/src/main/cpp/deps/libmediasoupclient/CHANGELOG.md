762168516c67a546955263c04d66014258992c8d
hezhubo <526125686@qq.com>
2025-11-04 01:17:59 +0800

    add mediasoup 3.4.3

---
+++ ohos_webrtc/src/main/cpp/deps/libmediasoupclient/CHANGELOG.md
@@ -0,0 +1,68 @@
+# Changelog
+
+
+### 3.4.3
+
+* Update to libwebrtc M120/6099 ([#173](https://github.com/versatica/libmediasoupclient/pull/173)). Thanks @Poldraunic, @janreyho, @copiltembel.
+
+### 3.4.2
+
+* Fix explicit codec selection ([#164](https://github.com/versatica/libmediasoupclient/pull/164)). Thanks @fedulvtubudul.
+
+
+### 3.4.1
+
+* Clear the stored transceivers before closing the PeerConnection ([#156](https://github.com/versatica/libmediasoupclient/pull/156)). Thanks @adriancretu.
+* Fix non-virtual destructors ([PR #157](https://github.com/versatica/libmediasoupclient/pull/157)). Thanks @adriancretu.
+* Fix min max range inclusion. ([PR #158](https://github.com/versatica/libmediasoupclient/pull/158)). Thanks @adriancretu.
+* Update libsdptransform to 1.2.10 ([PR #171](https://github.com/versatica/libmediasoupclient/pull/171)). Thanks@copiltembel.
+
+
+### 3.4.0
+
+* 'maxaveragebitrate' support for Opus. Thanks @PeterCang.
+*  Enable VP9 SVC ([#131](https://github.com/versatica/libmediasoupclient/pull/131)). Thanks @harvestsure.
+*  Reuse closed m= sections in remote SDP offer ([#99](https://github.com/versatica/libmediasoupclient/pull/99)).
+*  Allow forcing local DTLS role ([#133](https://github.com/versatica/libmediasoupclient/pull/133)).
+*  Add cbr config option for opus ([#138](https://github.com/versatica/libmediasoupclient/pull/138)). Thanks @GEverding.
+
+### 3.3.0
+
+* Update to libwebrtc M94/4606.
+
+### 3.2.0
+
+* Do not auto generate the stream ID for the receiving dataChannel,
+  but provide it via API. Fixes ([#126](https://github.com/versatica/libmediasoupclient/pull/126)).
+
+
+### 3.1.5
+
+* Fix profile-id codec parameter by converting parsed value into integer. Fixes ([#115](https://github.com/versatica/libmediasoupclient/pull/115))
+
+
+### 3.1.4
+
+* Convert `RecvTransport::Listener` to a subclass. Thanks @maxweisel.
+* Fix ambiguous cast error when compiling with MSVC. Thanks @maxweisel.
+
+
+### 3.1.3
+
+* Fix H264 `profile-level-id` value in SDP answer.
+  - Same as in https://github.com/versatica/mediasoup-client/issues/148
+
+
+### 3.1.2
+
+* Fix memory leak ([#105](https://github.com/versatica/libmediasoupclient/pull/105)). Thanks @ploverlake.
+
+
+### 3.1.1
+
+* Update `libsdptransform` dep to 1.2.8.
+
+
+### 3.1.0
+
+* DataChannel support.
---
+++ ohos_webrtc/src/main/cpp/deps/libmediasoupclient/test/src/MediaStreamTrackFactory.cpp
@@ -0,0 +1,118 @@
+#ifndef MEDIA_STREAM_TRACK_FACTORY_H
+#define MEDIA_STREAM_TRACK_FACTORY_H
+
+#define MSC_CLASS "MediaStreamTrackFactory"
+
+#include "MediaStreamTrackFactory.hpp"
+#include "MediaSoupClientErrors.hpp"
+#include "api/audio_codecs/builtin_audio_decoder_factory.h"
+#include "api/audio_codecs/builtin_audio_encoder_factory.h"
+#include "api/create_peerconnection_factory.h"
+#include "api/video_codecs/builtin_video_decoder_factory.h"
+#include "api/video_codecs/builtin_video_encoder_factory.h"
+#include "mediasoupclient.hpp"
+#include "pc/test/fake_audio_capture_module.h"
+#include "pc/test/fake_video_track_source.h"
+#include <iostream>
+
+using namespace mediasoupclient;
+
+void MediaStreamTrackFactory::Create()
+{
+	if (Factory)
+		return;
+	NetworkThread   = rtc::Thread::CreateWithSocketServer();
+	WorkerThread    = rtc::Thread::Create();
+	SignalingThread = rtc::Thread::Create();
+
+	NetworkThread->SetName("network_thread", nullptr);
+	SignalingThread->SetName("signaling_thread", nullptr);
+	WorkerThread->SetName("worker_thread", nullptr);
+
+	if (!NetworkThread->Start() || !SignalingThread->Start() || !WorkerThread->Start())
+	{
+		MSC_THROW_INVALID_STATE_ERROR("thread start errored");
+	}
+
+	webrtc::PeerConnectionInterface::RTCConfiguration config;
+
+	auto fakeAudioCaptureModule = FakeAudioCaptureModule::Create();
+	if (!fakeAudioCaptureModule)
+	{
+		MSC_THROW_INVALID_STATE_ERROR("audio capture module creation errored");
+	}
+
+	Factory = webrtc::CreatePeerConnectionFactory(
+	  NetworkThread.get(),
+	  WorkerThread.get(),
+	  SignalingThread.get(),
+	  fakeAudioCaptureModule,
+	  webrtc::CreateBuiltinAudioEncoderFactory(),
+	  webrtc::CreateBuiltinAudioDecoderFactory(),
+	  webrtc::CreateBuiltinVideoEncoderFactory(),
+	  webrtc::CreateBuiltinVideoDecoderFactory(),
+	  nullptr /*audio_mixer*/,
+	  nullptr /*audio_processing*/);
+
+	if (!Factory)
+	{
+		MSC_THROW_ERROR("error ocurred creating peerconnection factory");
+	}
+	PeerConnectionOptions.factory = Factory.get();
+}
+
+void MediaStreamTrackFactory::ReleaseThreads()
+{
+	if (Factory)
+	{
+		PeerConnectionOptions.factory = nullptr;
+		Factory                       = nullptr;
+	}
+
+	if (NetworkThread)
+	{
+		NetworkThread->Stop();
+		NetworkThread.reset();
+		NetworkThread = nullptr;
+	}
+
+	if (WorkerThread)
+	{
+		WorkerThread->Stop();
+		WorkerThread.reset();
+		WorkerThread = nullptr;
+	}
+
+	if (SignalingThread)
+	{
+		SignalingThread->Stop();
+		SignalingThread.reset();
+		SignalingThread = nullptr;
+	}
+}
+
+// Audio track creation.
+rtc::scoped_refptr<webrtc::AudioTrackInterface> createAudioTrack(const std::string& label)
+{
+	MediaStreamTrackFactory& singleton = MediaStreamTrackFactory::getInstance();
+
+	cricket::AudioOptions options;
+	options.highpass_filter = false;
+
+	rtc::scoped_refptr<webrtc::AudioSourceInterface> source =
+	  singleton.Factory->CreateAudioSource(options);
+
+	return singleton.Factory->CreateAudioTrack(label, source.get());
+}
+
+// Video track creation.
+rtc::scoped_refptr<webrtc::VideoTrackInterface> createVideoTrack(const std::string& label)
+{
+	MediaStreamTrackFactory& singleton = MediaStreamTrackFactory::getInstance();
+
+	rtc::scoped_refptr<webrtc::FakeVideoTrackSource> source = webrtc::FakeVideoTrackSource::Create();
+
+	return singleton.Factory->CreateVideoTrack(source, label);
+}
+
+#endif // MEDIA_STREAM_TRACK_FACTORY_H
